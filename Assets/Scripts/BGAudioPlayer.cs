344abf7d3e71d14963a9f4092a40e156da67f0b9
Dablestar <jbynchoi@gmail.com>
2024-09-24 00:51:58 +1000

    Audio source created

---
+++ Assets/Scripts/BGAudioPlayer.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGAudioPlayer : MonoBehaviour
+{
+    [SerializeField] private List<AudioClip> audioList;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/PacStudentController.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PacStudentController : MonoBehaviour
+{
+    [SerializeField] private List<AudioClip> footstepSoundEffects;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
