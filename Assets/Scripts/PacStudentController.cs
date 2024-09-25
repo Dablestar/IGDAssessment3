7949cd6cf7242876560e335982b51ec08ce1c04f
Dablestar <jbynchoi@gmail.com>
2024-09-25 16:27:55 +1000

    Spawner added

--- Assets/Scripts/PacStudentController.cs
+++ Assets/Scripts/PacStudentController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,57 +7,85 @@ public class PacStudentController : MonoBehaviour
 {
     [SerializeField] private float moveSpeed;
     [SerializeField] private List<AudioClip> footstepSoundEffects;
-    private Animator studentAnim;
+    [SerializeField] private AudioClip onPlayerHitSound;
+    [SerializeField] private Animator studentAnim;
+    private int life;
     static int score { get; set; }
 
     private AudioSource studentSound;
 
-    private List<AudioClip> footworkClips;
-
     private bool isWalking;
 
     private Tweener tweener;
-
-    private Vector3[] turningPoints = new Vector3[]
-    {
-        new Vector3(-8.5f, 3f),
-        new Vector3(0f, 3f),
-        new Vector3(0f, -3f),
-        new Vector3(-8.5f, -3f)
-    };
     
     // Start is called before the first frame update
     void Awake()
     {
         moveSpeed = 3f;
         score = 0;
-        studentAnim = GetComponent<Animator>();
+        studentSound = gameObject.GetComponent<AudioSource>();
+        Stop();
+        StartCoroutine(MovePlayer());
     }
 
     // Update is called once per frame
+    
+    //Raycast to check wall and floor,
+    //if wall detected, turn 90
     void Update()
     {
-        
+        PlayFootworkAudio();
     }
 
-    public static void addScore(int amount)
+    public static void AddScore(int amount)
     {
         score += amount;
     }
 
-    private void playFootworkAudio()
+    private void PlayFootworkAudio()
     {
-        
+        if (isWalking)
+        {
+            if (studentSound.clip == footstepSoundEffects[0])
+            {
+                studentSound.clip = footstepSoundEffects[1];
+                studentSound.Play();
+            }
+            else
+            {
+                studentSound.clip = footstepSoundEffects[0];
+                studentSound.Play();
+            }
+        }
+    }
+
+    private void Stop()
+    {
+        moveSpeed = 0f;
+        studentAnim.SetFloat("moveSpeed", 0);
+        isWalking = false;
     }
 
     private IEnumerator MovePlayer()
     {
-        for (int i = 0; i < turningPoints.Length; i++)
+        isWalking = true;
+        yield return new WaitForSeconds(1f);
+        isWalking = false;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag.Equals("Enemy"))
         {
-            studentAnim.SetFloat("moveSpeed", moveSpeed);
-            tweener.AddTween(gameObject.transform, gameObject.transform.position, turningPoints[i], moveSpeed);
-            studentAnim.SetFloat("moveSpeed", 0);
-            yield return new WaitForSeconds(0.5f);
+            Stop();
+            studentAnim.SetTrigger(3);
+            studentSound.clip = onPlayerHitSound;
+            studentSound.Play();
+            if (!ObjectSpawner.Respawn())
+            {
+                Destroy(this);        
+            }
+            
         }
     }
 }
--- Assets/Scripts/PalletManager.cs
+++ Assets/Scripts/PalletManager.cs
@@ -6,29 +6,36 @@ using UnityEngine;
 public class PalletManager : MonoBehaviour
 {
     private AudioSource source;
+
     [SerializeField] private AudioClip sound;
+
     // Start is called before the first frame update
     void Start()
     {
-        source = gameObject.GetComponent<AudioSource>();
+        source = GameObject.Find("FXPlayer").gameObject.GetComponent<AudioSource>();
     }
 
     private void OnCollisionEnter2D(Collision2D other)
     {
-        source.Play();
-        if (gameObject.tag.Equals("normalPallet"))
+        if (other.gameObject.tag.Equals("Player"))
         {
-            PacStudentController.addScore(500);
+            source.clip = sound;
+            source.Play();
+            if (gameObject.tag.Equals("normalPallet"))
+            {
+                PacStudentController.AddScore(500);
+            }
+
+            if (gameObject.tag.Equals("specialPallet"))
+            {
+                StartCoroutine(EnemyController.WeakenEnemy());
+            }
+
+            if (gameObject.tag.Equals("bonusPallet"))
+            {
+                PacStudentController.AddScore(5000);
+            }
+            Destroy(this);
         }
-        if (gameObject.tag.Equals("specialPallet"))
-        {
-            StartCoroutine(EnemyController.WeakenEnemy());
-        }
-        if (gameObject.tag.Equals("bonusPallet"))
-        {
-            PacStudentController.addScore(5000);
-        }
-        Destroy(this);
     }
-    
 }
