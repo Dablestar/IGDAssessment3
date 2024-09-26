97e20120d01be5cc9727f6575258d91d55ab6b51
Dablestar <jbynchoi@gmail.com>
2024-09-26 20:56:01 +1000

    Revert "Object Instantiation Completed"
    This reverts commit 675d22bf49995e4bfc1ede14e90470e9ec210739.

--- Assets/Scripts/LevelGenerator.cs
+++ Assets/Scripts/LevelGenerator.cs
@@ -29,7 +29,6 @@ public class LevelGenerator : MonoBehaviour
 
         wallParent.transform.position = new Vector3(40f, 0f, 0f);
         palletParent.transform.position = new Vector3(40f, 0f, 0f);
-        CreateMap();
     }
     
     public void GetMapInfoFromFile()
@@ -46,71 +45,33 @@ public class LevelGenerator : MonoBehaviour
 
     public void CreateMap()
     {
-        Vector3Int position = Vector3Int.FloorToInt(wallParent.transform.position);
-        int origin = position.x;
-        for (int y=0; y<mapInfo.Count; y++)
+        Vector3Int position = new Vector3Int(0, 0, 0);
+        foreach (string line in mapInfo)
         {
-            bool xStarted = false;
-            bool yStarted = false;
-            string[] line = mapInfo[y].Split(",");
-            Debug.Log(line.ToString());
-            for (int x=0; x<line.Length; x++)
+            foreach (string i in line.Split(","))
             {
-                Quaternion rotation = Quaternion.identity;
-                switch (line[x])
+                switch (i)
                 {
                     case "0":
-                        Instantiate(tilePalette[0], position, Quaternion.identity, wallParent.transform);
                         break;
                     case "1":
-                        if (yStarted)
-                        {
-                            if (xStarted)
-                            {
-                                
-                            }
-                        }
-                        else
-                        {
-                            if (xStarted)
-                            {
-                                
-                            }
-                        }
-                        Instantiate(tilePalette[1], position, rotation, wallParent.transform);
                         break;
                     case "2":
-                        if ((y == 0 || y == mapInfo.Count))
-                        {
-                            rotation *= Quaternion.Euler(0, 0, 90f);
-                        }
-                        Instantiate(tilePalette[2], position, rotation, wallParent.transform);
                         break;
                     case "3":
-                        Instantiate(tilePalette[3], position, Quaternion.identity, wallParent.transform);
                         break;
                     case "4":
-                        Instantiate(tilePalette[4], position, Quaternion.identity, wallParent.transform);
                         break;
                     case "5":
-                        Instantiate(tilePalette[0], position, Quaternion.identity, wallParent.transform);
-                        Instantiate(tilePalette[5], position, Quaternion.identity, palletParent.transform);
                         break;
                     case "6":
-                        Instantiate(tilePalette[0], position, Quaternion.identity, wallParent.transform);
-                        Instantiate(tilePalette[6], position, Quaternion.identity, palletParent.transform);
                         break;
                     case "7":
-                        Instantiate(tilePalette[7], position, Quaternion.identity, wallParent.transform);
                         break;
                     default:
                         break;
                 }
-                position += Vector3Int.right;
             }
-
-            position.x = origin;
-            position += Vector3Int.down;
         }
     }
 }
