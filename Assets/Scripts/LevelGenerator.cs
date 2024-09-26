8dae3164fae408509b9099e0de32250e2792d509
Dablestar <jbynchoi@gmail.com>
2024-09-27 03:04:28 +1000

    Neighbor Indexing and Border Exception Handling OnGoing

--- Assets/Scripts/LevelGenerator.cs
+++ Assets/Scripts/LevelGenerator.cs
@@ -50,139 +50,152 @@ public class LevelGenerator : MonoBehaviour
         {
             for (int x = 0; x < mapInfo[y].Length; x++)
             {
+                string upNeighbor = y > 0 ? mapInfo[y - 1][x] : "0";
+                string downNeighbor = y < mapInfo.Count - 1 ? mapInfo[y + 1][x] : "0";
+                string leftNeighbor = x > 0 ? mapInfo[y][x - 1] : "0";
+                string rightNeighbor = x < mapInfo[y].Length - 1 ? mapInfo[y][x + 1] : "0";
+
                 Quaternion rotation = Quaternion.identity;
                 switch (mapInfo[y][x])
                 {
                     case "0":
+                        Debug.Log($"x : {x}, y: {y}");
                         Instantiate(tilePalette[0], position, Quaternion.identity, wallParent.transform);
                         break;
-                    case "1":
-                        if (y == 0)
-                        {
-                            //case : y == 0
-                            if (x == 0)
-                            {
-                                //case : origin
-                                if (mapInfo[y + 1][x].Equals("2") && mapInfo[y][x + 1].Equals("2"))
-                                {
-                                    rotation *= Quaternion.Euler(0, 0, 270);
-                                }
-                            }
-                            else if (x == mapInfo[y].Length)
-                            {
-                                //case : last line 
-                                if (mapInfo[y][x - 1].Equals("2") && mapInfo[y + 1][x].Equals("2"))
-                                {
-                                    rotation *= Quaternion.Euler(0, 0, 180);
-                                }
-                            }
-                        }
-                        else if (y == mapInfo.Count)
-                        {
-                            //case : y == last line
-                            if (x == 0)
-                            {
-                                //case : origin
-                                if (mapInfo[y - 1][x].Equals("2") && mapInfo[y][x + 1].Equals("2"))
-                                {
-                                    rotation *= Quaternion.Euler(0, 0, 90);
-                                }
-                            }
-                            else if (x == mapInfo[y].Length)
-                            {
-                                //case : last line 
-                                if (mapInfo[y][x - 1].Equals("2") && mapInfo[y - 1][x].Equals("2"))
-                                {
-                                    rotation *= Quaternion.Euler(0, 0, 180);
-                                }
-                            }
-                        }
-                        else
-                        {
-                            if (x == 0)
-                            {
-                                if ((mapInfo[y - 1][x].Equals("2") || mapInfo[y - 1][x].Equals("3")) &&
-                                    (mapInfo[y][x + 1].Equals("2") || mapInfo[y][x + 1].Equals("3")))
-                                {
-                                    rotation = Quaternion.identity;
-                                }
-
-                                if ((mapInfo[y + 1][x].Equals("2") || mapInfo[y + 1][x].Equals("3")) &&
-                                    (mapInfo[y][x + 1].Equals("2") || mapInfo[y][x + 1].Equals("3")))
-                                {
-                                    rotation *= Quaternion.Euler(0, 0, 90);
-                                }
-                            }
-                            else if (x == mapInfo[y].Length)
-                            {
-                                //case : last line 
-                                if ((mapInfo[y - 1][x].Equals("2") || mapInfo[y - 1][x].Equals("3")) &&
-                                    (mapInfo[y][x - 1].Equals("2") || mapInfo[y][x - 1].Equals("3")))
-                                {
-                                    rotation *= Quaternion.Euler(0, 0, 270);
-                                }
-
-                                if ((mapInfo[y + 1][x].Equals("2") || mapInfo[y + 1][x].Equals("3")) &&
-                                    (mapInfo[y][x - 1].Equals("2") || mapInfo[y][x - 1].Equals("3")))
-                                {
-                                    rotation *= Quaternion.Euler(0, 0, 180);
-                                }
-                            }
-                            else
-                            {
-                                if ((mapInfo[y - 1][x].Equals("2") || mapInfo[y - 1][x].Equals("3")) &&
-                                    (mapInfo[y][x + 1].Equals("2") || mapInfo[y][x + 1].Equals("3")))
-                                {
-                                    rotation = Quaternion.identity;
-                                }
-
-                                if ((mapInfo[y - 1][x].Equals("2") || mapInfo[y - 1][x].Equals("3")) &&
-                                    (mapInfo[y][x - 1].Equals("2") || mapInfo[y][x - 1].Equals("3")))
-                                {
-                                    rotation *= Quaternion.Euler(0, 0, 270);
-                                }
-
-                                if ((mapInfo[y + 1][x].Equals("2") || mapInfo[y + 1][x].Equals("3")) &&
-                                    (mapInfo[y][x + 1].Equals("2") || mapInfo[y][x + 1].Equals("3")))
-                                {
-                                    rotation *= Quaternion.Euler(0, 0, 90);
-                                }
-
-                                if ((mapInfo[y + 1][x].Equals("2") || mapInfo[y + 1][x].Equals("3")) &&
-                                    (mapInfo[y][x - 1].Equals("2") || mapInfo[y][x - 1].Equals("3")))
-                                {
-                                    rotation *= Quaternion.Euler(0, 0, 180);
-                                }
-                            }
-                        }
-
-                        Instantiate(tilePalette[1], position, rotation, wallParent.transform);
-                        break;
+                   
                     case "2":
+                        Debug.Log($"x : {x}, y: {y}");
                         //Base case : border
                         if ((y == 0 || y == mapInfo.Count))
                         {
                             rotation *= Quaternion.Euler(0, 0, 90f);
+                            Instantiate(tilePalette[2], position, rotation, wallParent.transform);
+                            break;
                         }
 
+                        if (x == 0)
+                        {
+                            if (mapInfo[y][x + 1].Equals("2"))
+                            {
+                                rotation *= Quaternion.Euler(0, 0, 90f);
+                                Instantiate(tilePalette[2], position, rotation, wallParent.transform);
+                                break;
+                            }
+                        }
+                        else if (x == mapInfo[y].Length - 1)
+                        {
+                            if (mapInfo[y][x - 1].Equals("2"))
+                            {
+                                rotation *= Quaternion.Euler(0, 0, 90f);
+                                Instantiate(tilePalette[2], position, rotation, wallParent.transform);
+                                break;
+                            }
+                        }
+
+                        if (x != 0 && x != mapInfo[y].Length - 1)
+                        {
+                            if ((mapInfo[y][x - 1].Equals("3") || mapInfo[y][x - 1].Equals("2")) ||
+                                (mapInfo[y][x + 1].Equals("3") || mapInfo[y][x + 1].Equals("2")))
+                            {
+                                rotation *= Quaternion.Euler(0, 0, 90f);
+                                Instantiate(tilePalette[2], position, rotation, wallParent.transform);
+                                break;
+                            }
+                        }
+
+                        
                         Instantiate(tilePalette[2], position, rotation, wallParent.transform);
                         break;
+                    case "1":
                     case "3":
-                        Instantiate(tilePalette[3], position, Quaternion.identity, wallParent.transform);
+                        if (rightNeighbor.Equals("2") || rightNeighbor.Equals("4"))
+                        {
+                            if ((downNeighbor.Equals("2") || downNeighbor.Equals("4")))
+                                rotation *= Quaternion.Euler(0, 0, 0);
+                            else if (upNeighbor.Equals("4")|| upNeighbor.Equals("4"))
+                                rotation *= Quaternion.Euler(0, 0, 270);
+                        }
+                        else if ((leftNeighbor.Equals("2") || leftNeighbor.Equals("4")))
+                        {
+                            if (downNeighbor.Equals("2")  || downNeighbor.Equals("4"))
+                                rotation *= Quaternion.Euler(0, 0, 90);
+                            else if (upNeighbor.Equals("2") || upNeighbor.Equals("4"))
+                                rotation *= Quaternion.Euler(0, 0, 180);
+                        }
+                        Instantiate(tilePalette[1], position, rotation, wallParent.transform);
                         break;
+                        
                     case "4":
+                        Debug.Log($"x : {x}, y: {y}");
+                        //Base case : border
+                        if ((y == 0 || y == mapInfo.Count))
+                        {
+                            rotation *= Quaternion.Euler(0, 0, 90f);
+                            Instantiate(tilePalette[4], position, rotation, wallParent.transform);
+                            break;
+                        }
+
+                        if (x == 0)
+                        {
+                            if (mapInfo[y][x + 1].Equals("2"))
+                            {
+                                rotation *= Quaternion.Euler(0, 0, 90f);
+                                Instantiate(tilePalette[4], position, rotation, wallParent.transform);
+                                break;
+                            }
+                        }
+
+                        if (x == mapInfo[y].Length - 1)
+                        {
+                            if (mapInfo[y][x - 1].Equals("2"))
+                            {
+                                rotation *= Quaternion.Euler(0, 0, 90f);
+                                Instantiate(tilePalette[4], position, rotation, wallParent.transform);
+                                break;
+                            }
+                        }
+                        else
+                        {
+                            if ((mapInfo[y][x - 1].Equals("3") || mapInfo[y][x - 1].Equals("2")) ||
+                                (mapInfo[y][x + 1].Equals("3") || mapInfo[y][x + 1].Equals("2")))
+                            {
+                                rotation *= Quaternion.Euler(0, 0, 90f);
+                                Instantiate(tilePalette[4], position, rotation, wallParent.transform);
+                                break;
+                            }
+                        }
+
                         Instantiate(tilePalette[4], position, Quaternion.identity, wallParent.transform);
                         break;
                     case "5":
+                        Debug.Log($"x : {x}, y: {y}");
                         Instantiate(tilePalette[0], position, Quaternion.identity, wallParent.transform);
                         Instantiate(tilePalette[5], position, Quaternion.identity, palletParent.transform);
                         break;
                     case "6":
+                        Debug.Log($"x : {x}, y: {y}");
                         Instantiate(tilePalette[0], position, Quaternion.identity, wallParent.transform);
                         Instantiate(tilePalette[6], position, Quaternion.identity, palletParent.transform);
                         break;
                     case "7":
-                        Instantiate(tilePalette[7], position, Quaternion.identity, wallParent.transform);
+                        Debug.Log($"x : {x}, y: {y}");
+                        if ((y == 0 || y == mapInfo.Count))
+                        {
+                            rotation *= Quaternion.Euler(0, 0, -90f);
+                        }
+                        else
+                        {
+                            if (x != 0 && x != mapInfo[y].Length - 1)
+                            {
+                                if ((mapInfo[y - 1][x].Equals("0") || mapInfo[y][x - 1].Equals("2")) ||
+                                    (mapInfo[y + 1][x].Equals("0") || mapInfo[y][x + 1].Equals("2")))
+                                {
+                                    rotation *= Quaternion.Euler(0, 0, -90f);
+                                }
+                            }
+                        }
+
+                        Instantiate(tilePalette[7], position, rotation, wallParent.transform);
                         break;
                     default:
                         break;
